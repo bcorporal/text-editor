7d3183cd1bc34741c304bb2f1e8eb3bcac91f708
bcorporal <bcorporal@gmail.com>
2022-05-11 23:17:52 -0400

    redo heroku

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,7 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
+    
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
